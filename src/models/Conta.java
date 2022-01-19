f00181f9e64594353fbaac8d1d7479d4f2ece569
Wesly <weslyramalho@yahoo.com.br>
2022-01-19 14:16:15 -0300

    Foi acrescentado a class cliente em conta

--- src/models/Conta.java
+++ src/models/Conta.java
@@ -1,43 +1,21 @@
 package models;
 
 public abstract class Conta {
-    private int id;
     private int numero;
     protected double saldo;
 
-    public int getId() {
-        return id;
-    }
+    private Cliente cliente;
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-
-    public Conta(int id, int numero, double saldo) {
-        this.id = id;
+    public Conta(int numero, double saldo, Cliente cliente) {
         this.numero = numero;
         this.saldo = saldo;
-    }
-
-
-    public double getSaldo(){
-        return this.saldo;
+        this.cliente = cliente;
     }
 
     @Override
     public String toString() {
         return "Conta{" +
                 "numero=" + numero +
-                ", id=" + id +
                 ", saldo=" + saldo +
                 '}';
     }
